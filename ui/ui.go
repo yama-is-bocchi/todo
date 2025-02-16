88966d8e1c06b68a8a388b598e800f6638d1e98d
yamaa <yamaa.com987654321@outlook.jp>
2025-02-16 03:25:50 +0000

    fontProperty追加

--- ui/ui.go
+++ ui/ui.go
@@ -8,9 +8,18 @@ type UserInterface interface {
 	Render(int, []string)
 }
 
+// ユーザーインターフェース
 type ui struct {
 	mainColor termbox.Attribute
 	subColor  termbox.Attribute
+}
+
+// フォントプロパティ
+type fontProperty struct {
+	x, y            int               // 位置
+	fontColor       termbox.Attribute // フォントカラー
+	backgroundColor termbox.Attribute // 背景色
+	message         string            // メッセージ内容
 }
 
 func NewUI(mainColor, subColor termbox.Attribute) *ui {
@@ -20,21 +29,44 @@ func NewUI(mainColor, subColor termbox.Attribute) *ui {
 // 現在の行の情報を反映して引数の行をレンダリングする.
 func (ui ui) Render(currentLine int, lines []string) {
 	termbox.Clear(ui.mainColor, ui.subColor)
-	for i, item := range lines {
-		fg := ui.mainColor
-		bg := ui.subColor
-		if i == currentLine {
-			fg, bg = ui.subColor, ui.mainColor
-		}
+	fonts := ui.setCurrentLine(currentLine, ui.generateFont(lines...))
+	for _, item := range fonts {
 		// setCell
-		ui.print(0, i, fg, bg, item)
+		ui.print(item)
 	}
 	termbox.Flush()
 }
 
 // 引数の情報で文字列をセルにセットする.
-func (ui ui) print(x, y int, fg, bg termbox.Attribute, msg string) {
-	for i, r := range msg {
-		termbox.SetCell(x+i, y, r, fg, bg)
+func (ui ui) print(font fontProperty) {
+	for i, r := range font.message {
+		termbox.SetCell(font.x+i, font.y, r, font.fontColor, font.backgroundColor)
 	}
+}
+
+// 引数の文字列のfont型を生成する
+func (ui ui) generateFont(lines ...string) []fontProperty {
+	var result []fontProperty
+	for y, line := range lines {
+		result = append(
+			result,
+			fontProperty{
+				x: 0, y: y,
+				fontColor:       ui.mainColor,
+				backgroundColor: ui.subColor,
+				message:         line,
+			})
+	}
+	return result
+}
+
+// 現在位置の行の色を反転する.
+func (ui ui) setCurrentLine(currentLine int, fonts []fontProperty) []fontProperty {
+	for i, font := range fonts {
+		if font.y == currentLine {
+			fonts[i] = fontProperty{font.x, font.y, font.backgroundColor, font.fontColor, font.message}
+			break
+		}
+	}
+	return fonts
 }
